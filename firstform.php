c484c03b3712b8ddeaf2078a444a4fa87b50f244
Gbaye Eunice <101324603+Euniceabi@users.noreply.github.com>
2022-06-13 04:41:41 +0100

    Add files via upload

---
+++ firstform.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html
+<html lang="en">
+    <head>
+        <title>form post</title>
+</head>
+<body>
+    <form method="POST" action="user_data.php">
+        <label for="name">
+            Name: <input type="text" name="name">
+        </label> <br>
+        <label for="email">
+            Email: <input type="email" name="email">
+        </label> <br>
+        <label for="date of birth">
+           Date of birth: <input type="date" name="date of birth">
+        </label> <br>
+        <label for="gender">
+           Gender: <input type="text" name="gender">
+        </label> <br>
+        <label for="country">
+          Country: <input type="text" name="country">
+        </label> <br>
+        
+        
+        <button type="submit">Submit</button>
+
+    </form>
+  
+</body> 
+</html>
+ 
